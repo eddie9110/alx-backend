ef69c6bbaa50338f718c017276980d5fedceb28a
eddie9110 <edwinmaregwa@gmail.com>
2024-04-04 12:02:43 +0000

    pycodetyle compliant

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,7 +7,7 @@ from typing import Tuple
 
 def index_range(page, page_size) -> Tuple[int, int]:
     """ function returns a Tuple of size two containing a start index
-    and an end index corresponding to the range of indexes to return 
+    and an end index corresponding to the range of indexes to return
     in a list for those particular pagination parameters."""
     current = page_size * page - page_size
     return (current, (page_size * page))
