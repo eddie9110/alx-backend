4c2f9dbe497576514be7bb63e9062dc328ec3633
eddie9110 <edwinmaregwa@gmail.com>
2024-04-04 13:03:48 +0000

    doc string added

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -6,7 +6,7 @@ from typing import List, Tuple, Dict
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """"""
+    """Simple helper function"""
     current = page_size * page - page_size
     return (current, (page_size * page))
 
@@ -47,14 +47,7 @@ class Server:
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
 
         """
-        method returns a dictionary containing the following key-value pairs:
-
-        page_size: the length of the returned dataset page
-        page: the current page number
-        data: the dataset page (equivalent to return from previous task)
-        next_page: number of the next page, None if no next page
-        prev_page: number of the previous page, None if no previous page
-        total_pages: the total number of pages in the dataset as an integer
+        method returns a dictionary containing key-value pairs
 
         """
         data = self.get_page(page, page_size)
