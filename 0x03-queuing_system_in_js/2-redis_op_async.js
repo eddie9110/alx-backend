6e3c804110e56cdae726aff48ff18d13d8784e79
eddie9110 <edwinmaregwa@gmail.com>
2024-05-07 08:47:38 +0000

    file added

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,25 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+const client = redis.createClient();
+const getProm = promisify(client.get).bind(client);
+
+client.on('error', (error) => {
+  if (error) console.log(`Redis client not connected to the server: ${error}`)
+}).on('connect', () => {
+    console.log('Redis client connected to the server');
+});
+
+
+function setNewSchool(schoolName, value) {
+    client.set(schoolName, value, redis.print);
+}
+
+async function displaySchoolValue(schoolName) {
+  const res = await getProm(schoolName);
+  console.log(res);
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
